16b5857d7c45464445534758fd92435a12e91d6b
Magdalena Struniawska <mag.sok@onet.pl>
2022-04-25 09:11:12 +0200

    Initial Commit

---
+++ CoreDataProjekt/CoreDataProjektApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjektApp.swift
+//  CoreDataProjekt
+//
+//  Created by Slawek on 25/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjektApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
